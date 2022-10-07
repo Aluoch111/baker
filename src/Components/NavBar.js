06eed04d0a178cedea07649930c134a131cb2237
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-10-07 12:56:47 +0300

    modify nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,29 @@
+ import React, { useEffect } from "react";
+ import { Link } from "react-router-dom";
 
-  import React, { useEffect } from "react";
+ function NavBar () {
 
-  export default function NavBar(){
-     
-  useEffect(() => {
-   fetch("https://bakers-delight.herokuapp.com/recipes")
- 
-   .then((res) => res.json())
-   .then( data => console.log(data));
-  } ,[] )
-     return (
-         
-             <div>
-                 <ul>
-                     <li>Home</li>
-                     <li>Recipe</li>
-                     <li>Add yours</li>
-                     <li>Login</li>
-                 </ul>
-             </div>
-          
-     );
+    useEffect(() => {
+        fetch("https://bakers-delight.herokuapp.com/recipes")
+      
+        .then((res) => res.json())
+        .then( data => console.log(data));
+       } ,[] )
+
+   return (
+
+     <nav className="navbar">
+       <h1>Baker's Delight</h1>
+       <div className="links">
+         <Link to="/">Home</Link>
+         <Link to="/newRecipe" style={{ 
+           color: 'white', 
+           backgroundColor: '#f1356d',
+           borderRadius: '8px' 
+         }}>New Recipe</Link>
+       </div>
+     </nav>
+   );
  }
+  
+ export default NavBar;
