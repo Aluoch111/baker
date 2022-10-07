e3665480fdb0ff36db02805e46304da0aaba5823
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-10-07 14:18:42 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import NavBar from "./Components/NavBar";
 import NewRecipe from "./Components/NewRecipe";
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
-import Home from "./Components/Home";
+import {  Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <Routes>
-      <Route path ="/" element = { <Home/>}/>
-      <Route path= "/navbar" element = {<NavBar/>}/>
+      <Route path= "/" element = {<NavBar/>}/>
       <Route path= "/newRecipe" element = { <NewRecipe/>}/>
       {/* <Route/>
       <Route/>
